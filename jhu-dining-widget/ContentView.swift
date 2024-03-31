a22671c57439825ba585227b880ceaa3b13b1cdb
tommix626 <xwang397@jh.edu>
2024-03-31 15:41:05 -0400

    Initial Commit

---
+++ jhu-dining-widget/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  jhu-dining-widget
+//
+//  Created by XH Wang on 2024/3/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ jhu-dining-widget/jhu_dining_widgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  jhu_dining_widgetApp.swift
+//  jhu-dining-widget
+//
+//  Created by XH Wang on 2024/3/31.
+//
+
+import SwiftUI
+
+@main
+struct jhu_dining_widgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
